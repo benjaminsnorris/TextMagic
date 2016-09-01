8e9b1e66dd9eca0bb29d9cb649e6fd3bda831f92
Ben Norris <code@bsn.design>
2016-09-01 07:07:34 -0600

    Add empty framework

---
+++ TextMagic/TextMagic/TextMagic.h
@@ -0,0 +1,19 @@
+//
+//  TextMagic.h
+//  TextMagic
+//
+//  Created by Ben Norris on 9/1/16.
+//  Copyright © 2016 BSN Design. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for TextMagic.
+FOUNDATION_EXPORT double TextMagicVersionNumber;
+
+//! Project version string for TextMagic.
+FOUNDATION_EXPORT const unsigned char TextMagicVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <TextMagic/PublicHeader.h>
+
+
